9cd819e716aa1c526b9294eee10bde146b9887b7
Modernize Studio <studio@modernize.com>
2026-07-03 07:04:40 +0000

    Commit for planId: 191

--- src/main/java/org/eclipse/cargotracker/interfaces/handling/file/FileProcessorJobListener.java
+++ src/main/java/org/eclipse/cargotracker/interfaces/handling/file/FileProcessorJobListener.java
@@ -1,26 +1,24 @@
 package org.eclipse.cargotracker.interfaces.handling.file;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import jakarta.batch.api.listener.JobListener;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 @Dependent
 @Named("FileProcessorJobListener")
-public class FileProcessorJobListener implements JobListener {
+public class FileProcessorJobListener {
 
-  @Inject private Logger logger;
+    @Inject
+    private Logger logger;
 
-  @Override
-  public void beforeJob() throws Exception {
-    logger.log(Level.INFO, "Handling event file processor batch job starting at {0}", new Date());
-  }
+    public void beforeJob() {
+        logger.log(Level.INFO, "Handling event file processor batch job starting at {0}", LocalDateTime.now());
+    }
 
-  @Override
-  public void afterJob() throws Exception {
-    logger.log(Level.INFO, "Handling event file processor batch job completed at {0}", new Date());
-  }
+    public void afterJob() {
+        logger.log(Level.INFO, "Handling event file processor batch job completed at {0}", LocalDateTime.now());
+    }
 }
